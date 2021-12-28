f46199fbd27be6512e7df437cb71e508e03a7329
nous <okmsnrn@gmail.com>
2021-12-28 17:40:16 +0100

    form validation

--- js/form.js
+++ js/form.js
@@ -1,56 +1,63 @@
-// Contact Form Validation
+ const form = document.getElementById('form');
+ const password1El = document.getElementById('password1');
+ const password2El = document.getElementById('password2');
+ const messageContainer = document.querySelector('.message-contaoner');
+ const message = document.getElementById('message');
 
-function animatedForm() {
-    const arrows = document.querySelectorAll(".fa-arrow-down");
-  
-    arrows.forEach(arrow => {
-      arrow.addEventListener('click', () => {
-        const input = arrow.previousElementSibling;
-        const parent = arrow.parentElement;
-        const nextForm = parent.nextElementSibling;
-  
-        // check for validation
-  
-        if (input.type === "text" && validateUser(input)) {
-          nextSlide(parent, nextForm);
-        } else if (input.type === 'email' && validateEmail(input)) {
-          nextSlide(parent, nextForm);
-        } else if (input.type === 'text' && validateUser(input)) {
-          nextSlide(parent, nextForm);
-        }
-      });
-    });
+
+ let isValid = false;
+ let passwordsMatch = false;
+
+function validateForm() {
+//Using Constraint API
+isValid = form.checkValidity();
+//Style main message for an error
+if (!isValid) {
+  message.textContent = 'Please fill out all fields';
+  message.style.color = 'red';
+  return;
+}
+//Check to see if passwords match
+if (password1El.value === password2El.value) {
+  passwordsMatch = true;
+  password1El.style.borderColor = 'green';
+  password2El.style.borderColor = 'green';
+} else {
+  passwordsMatch = false;
+  message.textContent = 'Make sure passwords match';
+  message.style.color = 'red';
+  password1El.style.borderColor = 'red';
+  password2El.style.borderColor = 'red';
+  return;
+}
+//If form is valid and paswords match
+if (isValid && passwordsMatch) {
+message.textContent = 'Successfully Registered!';
+message.style.color = 'green';
+}
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    password: form.password.value
+  };
+  //Something useful doing with userData: sending to server z.B.
+  console.log(user);
+}
+
+ function processFormData(e) {
+   e.preventDefault();
+  //Validate Form
+  validateForm();
+  //Submit Data if Valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
   }
-  
-  function validateUser(user){
-      if(user.value.length < 5){
-        alert('not enough characters');
-        error('rgb(189, 87, 87)');
-      } else {
-        return true;
-      }
-  }
-  
-  function validateEmail(email) {
-    const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (validation.test(email.value)) {
-      return true;
-    } else {
-      error('rgb(189, 87, 87)');
-    }
-  }
-  
-  function nextSlide(parent, nextForm){
-    parent.classList.add('innactive');
-    parent.classList.remove('active');
-    nextForm.classList.add('active');
-  }
-  
-  
-  
-  function error(color){
-    document.body.style.backgroundColor = color;
-  }
-  
-  animatedForm();
-  
+ }
+
+ //Event Listener
+
+ form.addEventListener('submit', processFormData);
